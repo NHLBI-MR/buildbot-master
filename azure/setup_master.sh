21d6a8eba6e5885c5abc7688713963d81bb4b0d3
Michael S. Hansen <michael.hansen@nih.gov>
2017-06-02 10:28:49 -0400

    Fixed permissions and bugs in setup script.

--- azure/setup_master.sh
+++ azure/setup_master.sh
@@ -6,7 +6,9 @@ pip install --upgrade pip
 pip install 'buildbot[bundle]'
 
 buildbot create-master /home/buildbot/master
-cp /home/buildbot/master/master.cfg.example /home/buildbot/master/master.cfg
+cp /home/buildbot/master/master.cfg.sample /home/buildbot/master/master.cfg
+
+chown -R buildbot:buildbot /home/buildbot/master
 
 cp buildbot-master.service /etc/systemd/system/
 service buildbot-master start
